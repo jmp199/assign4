859bffa6cfac3dd336c2c606ce125bcf13ec16e4
jmp199 <65424990+jmp199@users.noreply.github.com>
2020-05-24 21:45:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-var welcome ="My favorite color is";
-//var demo = function () {
-  //document.getElementById("demo").innerHTML = "blue";
-//};
-document.querySelector(".welcome").innerHTML = welcome;
+var number = 2;
 
-var color = "purple";
 
-  document.getElementById("myBtn").addEventListener("click", function(){
-  document.getElementById("demo").innerHTML = color;
-  document.getElementById("demo").style.color = "black";
-});
-document.getElementById("demo").addEventListener("mouseover", function(){
-  document.getElementById("demo").innerHTML = color;
-  document.getElementById("demo").style.color = color;
-});
-document.getElementById("demo").addEventListener("mouseout", function(){
-  document.getElementById("demo").innerHTML = color;
-  document.getElementById("demo").style.color = "black";
-});
 
-writeHTML;
+if (number == 2 ) {
+ 
+  number = number + "Number is Even";
+  document.querySelector(".condition").innerHTML = number;
+} else {
+  document.querySelector(".condition").innerHTML = number;
+}
+
+
+var number = 2;
+
+
+
+if (number == 2 ) {
+ 
+  number = number + "Number is positive";
+  document.querySelector(".condition2").innerHTML = number;
+} else {
+  document.querySelector(".condition2").innerHTML = number;
+}
